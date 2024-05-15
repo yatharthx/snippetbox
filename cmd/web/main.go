15f2dfbf9dee370b4c61308feea14bf6335ee1dd
Yatharth K <yatharth01@gmail.com>
2024-05-15 10:23:37 +0530

    structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 	// command line flags
 	addr := flag.String("addr", ":3000", "HTTP network address")
 	flag.Parse()
+
+	// logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// initialize an instance of application struct
+	app := &application{
+		logger: logger,
+	}
 
 	// instantiate servers
 	mux := http.NewServeMux()
@@ -18,13 +31,14 @@ func main() {
 	// register routes
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	// start the server
-	log.Printf("starting server on %s", *addr)
+	logger.Info("starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
