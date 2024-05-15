349966330accc003dc06eeefc36b4225210d2e67
Yatharth K <yatharth01@gmail.com>
2024-05-15 10:38:36 +0530

    refactoring for routes registration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,33 +12,18 @@ type application struct {
 }
 
 func main() {
-	// command line flags
 	addr := flag.String("addr", ":3000", "HTTP network address")
 	flag.Parse()
 
-	// logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// initialize an instance of application struct
 	app := &application{
 		logger: logger,
 	}
 
-	// instantiate servers
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// register routes
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-	// start the server
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
