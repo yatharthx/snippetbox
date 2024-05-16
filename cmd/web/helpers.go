860f79c4126730fa2a14d13c65e2240010b8b9ac
Yatharth K <yatharth01@gmail.com>
2024-05-16 22:12:17 +0530

    template error handling

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
@@ -27,10 +28,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
 	err := ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
